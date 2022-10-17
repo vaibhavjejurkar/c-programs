c3698855c0a9de1483493c7a5be3cca1e9cc56f4
vaibhavjejurkar <115398438+vaibhavjejurkar@users.noreply.github.com>
2022-10-17 22:15:11 +0530

    Add files via upload

---
+++ Assingment110.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int Day,Month,Year;
+    Printf("Enter DD/MM/YYYY in format:");
+    scanf("%d/%d/%d",&Day,&Month,&Year);
+
+    printf("Day-%d,Month-%d,Year-%d",Day,Month,Year);
+
+    return 0; 
+}
